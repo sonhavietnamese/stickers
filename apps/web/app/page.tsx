251baad769969161322246314ce06cd3a2704cb4
sonhavietnamese <nguyensonha.hanz@gmail.com>
2024-06-19 01:08:06 +0700

    feat: add canvas

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,18 @@
-import { ModeToggle } from '@/components/mode-toggle'
-import { CounterButton } from '@repo/ui'
+'use client'
+
+import { Button } from '@/components/ui/button'
 import React from 'react'
+import { Tldraw } from 'tldraw'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between bg-background p-24'>
-      <ModeToggle />
-      <CounterButton />
+    <main className='w-screen h-screen'>
+      <div className='absolute z-10 bottom-4 left-3'>
+        <Button>asd</Button>
+      </div>
+      <div className='fixed inset-0'>
+        <Tldraw />
+      </div>
     </main>
   )
 }
