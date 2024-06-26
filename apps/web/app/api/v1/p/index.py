b731671427c0802c83e35c74a3b898cb5f0cbef3
sonhavietnamese <nguyensonha.hanz@gmail.com>
2024-06-27 03:11:29 +0700

    feat(outline): loaded

--- apps/web/app/api/v1/p/index.py
+++ apps/web/app/api/v1/p/index.py
@@ -14,6 +14,8 @@ class RemoveBackgroundRequest(BaseModel):
 MODEL_NAME = "u2net"
 session = new_session(MODEL_NAME)
 
+IMG_SIZE = 256
+
 @app.route("/api/v1/p", methods = ['GET'])
 def health_check():
     return {"message": "Server is up and running!"}
@@ -32,12 +34,22 @@ def remove_background():
     except base64.binascii.Error as e:
         return {"image": None, "error": "Invalid base64 format. Image must be a valid base64 encoded string."}
 
+   # Open the image
     img = Image.open(BytesIO(img_data))
-    img_resized = img.resize((512, 512))
-    output_removed = remove(img_resized, session=session,
-    post_process_mask=True)
+
+   # Calculate the height that maintains the aspect ratio
+    width_percent = (IMG_SIZE / float(img.size[0]))
+    height_size = int((float(img.size[1]) * float(width_percent)))
+
+   # Resize the image
+    img_resized = img.resize((IMG_SIZE, height_size))
+    output_removed = remove(
+        img_resized,
+        session=session,
+        post_process_mask=True)
+    output_cropped = output_removed.crop(output_removed.getbbox())
     buffered = BytesIO()
-    output_removed.save(buffered, format="PNG")
+    output_cropped.save(buffered, format="PNG")
 
     return {"image": base64.b64encode(buffered.getvalue()).decode()}
 
--- apps/web/app/me/page.tsx
+++ apps/web/app/me/page.tsx
@@ -1,206 +1,207 @@
-'use client'
+"use client"
 
-import Image from 'next/image'
-import { useCallback, useEffect } from 'react'
-import { type Editor, Tldraw, track, useEditor } from 'tldraw'
-import Marquee from "react-fast-marquee";
+import {outline} from "@/lib/utils"
+import NextImage from "next/image"
+import {useCallback} from "react"
+import Marquee from "react-fast-marquee"
+import {AssetRecordType, type Editor, Tldraw, getHashForString, track, uniqueId, useEditor, useTools,} from "tldraw"
+
+function fileToBase64(file: File): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.onloadend = () => {
+      resolve(reader.result as string)
+    }
+    reader.onerror = reject
+    reader.readAsDataURL(file)
+  })
+}
+
+async function loadImage(base64Image: string): Promise<HTMLImageElement> {
+  return new Promise<HTMLImageElement>((resolve, reject) => {
+    console.log(base64Image)
+
+    const img = new Image()
+    img.crossOrigin = "anonymous"
+    img.onload = () => {
+      try {
+        resolve(img)
+      } catch (error) {
+        reject(error)
+      }
+    }
+    img.onerror = reject
+    img.src = base64Image
+  })
+}
 
 function CustomUiExample() {
   const handleMount = useCallback((editor: Editor) => {
-    console.log('editor', editor)
+    editor.registerExternalAssetHandler("file", async ({ file }) => {
+      const id = uniqueId()
+      const url = "api/v1/p"
+      const base64String = await fileToBase64(file)
+      const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+          image: base64String.split(",")[1],
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
 
-    editor.setCurrentTool('draw')
+      const data = await response.json()
+      const loadedImage = await loadImage(`data:image/png;base64,${data.image}`)
 
-    // editor.addListener("")
+      const outlined = outline(loadedImage, 10, "black").toDataURL()
+      const loadedOutlined = await loadImage(outlined)
 
-    // editor.asset
-
-    // editor.registerExternalContentHandler('text', async ({ point, sources }) => {
-    //   console.log('sources', sources)
-    //   console.log('point', point)
-
-    //   const htmlSource = sources?.find((s) => s.type === 'text' && s.subtype === 'html')
-
-    //   if (htmlSource) {
-    //     const center = point ?? editor.getViewportPageBounds().center
-
-    //     editor.createShape({
-    //       type: 'html',
-    //       x: center.x - 250,
-    //       y: center.y - 150,
-    //       props: {
-    //         html: htmlSource.data,
-    //       },
-    //     })
-    //   }
-    // })
-
-    // [a]
-    // editor.registerExternalAssetHandler('file', async ({ file }: { type: 'file'; file: File }) => {
-    //   const id = uniqueId()
-
-    //   const objectName = `${id}-${file.name}`.replaceAll(/[^a-zA-Z0-9.]/g, '-')
-
-    //   console.log('objectName', objectName)
-
-    //   // const url = `${UPLOAD_URL}/${objectName}`
-
-    //   // await fetch(url, {
-    //   //   method: 'POST',
-    //   //   body: file,
-    //   // })
-    //   // //[b]
-    //   // const assetId: TLAssetId = AssetRecordType.createId(getHashForString(url))
-
-    //   // let size: {
-    //   //   w: number
-    //   //   h: number
-    //   // }
-    //   // let isAnimated: boolean
-    //   // let shapeType: 'image' | 'video'
-
-    //   // //[c]
-    //   // if (MediaHelpers.isImageType(file.type)) {
-    //   //   shapeType = 'image'
-    //   //   size = await MediaHelpers.getImageSize(file)
-    //   //   isAnimated = await MediaHelpers.isAnimated(file)
-    //   // } else {
-    //   //   shapeType = 'video'
-    //   //   isAnimated = true
-    //   //   size = await MediaHelpers.getVideoSize(file)
-    //   // }
-    //   // //[d]
-    //   // const asset: TLAsset = AssetRecordType.create({
-    //   //   id: assetId,
-    //   //   type: shapeType,
-    //   //   typeName: 'asset',
-    //   //   props: {
-    //   //     name: file.name,
-    //   //     src: url,
-    //   //     w: size.w,
-    //   //     h: size.h,
-    //   //     fileSize: file.size,
-    //   //     mimeType: file.type,
-    //   //     isAnimated,
-    //   //   },
-    //   // })
-
-    //   return asset
-    // })
+      return AssetRecordType.create({
+        id: AssetRecordType.createId(getHashForString(id)),
+        type: "image",
+        typeName: "asset",
+        props: {
+          name: file.name,
+          src: loadedOutlined.src,
+          w: loadedOutlined.width,
+          h: loadedOutlined.height,
+          mimeType: file.type,
+          isAnimated: false,
+        },
+      })
+    })
   }, [])
 
-  return <Tldraw className='z-10' hideUi onMount={handleMount} />
+  return (
+    <Tldraw onMount={handleMount} hideUi={true} className={"z-10 absolute"}>
+      <CustomUi />
+    </Tldraw>
+  )
 }
 
 const CustomUi = track(() => {
+  const tools = useTools()
   const editor = useEditor()
 
-  useEffect(() => {
-    const handleKeyUp = (e: KeyboardEvent) => {
-      switch (e.key) {
-        case 'Delete':
-        case 'Backspace': {
-          editor.deleteShapes(editor.getSelectedShapeIds())
-          break
-        }
-        case 'v': {
-          editor.setCurrentTool('select')
-          break
-        }
-        case 'e': {
-          editor.setCurrentTool('eraser')
-          break
-        }
-        case 'x':
-        case 'p':
-        case 'b':
-        case 'd': {
-          editor.setCurrentTool('draw')
-          break
-        }
-      }
-    }
-
-    window.addEventListener('keyup', handleKeyUp)
-    return () => {
-      window.removeEventListener('keyup', handleKeyUp)
-    }
-  })
-
   return (
-    <div className='custom-toolbar h-[100px] w-[500px] bg-green-500 absolute z-[300] inset-0'>
-      <button
-        type='button'
-        className='text-black bg-red-300'
-        data-isactive={editor.getCurrentToolId() === 'image'}
-        onClick={() => {
-          editor.setCurrentTool('asset')
-        }}>
-        Pencil
-      </button>
+    <div className="custom-layout">
+      <div className="">
+        <button
+          type="button"
+          className="custom-button"
+          onClick={() => tools.asset.onSelect("toolbar")}
+        >
+          Select
+        </button>
+      </div>
     </div>
   )
 })
 
 export default function Page() {
   return (
-    <main className='w-screen h-screen leading-none relative overflow-hidden'>
-      <figure className='absolute z-30 left-1/2 -translate-x-1/2'>
-        <Image src={'/assets/logo.png'} className='w-[150px]' width={150} height={200} alt='' />
+    <main className="w-screen h-screen leading-none relative overflow-hidden">
+      <figure className="absolute z-30 left-1/2 -translate-x-1/2">
+        <NextImage
+          src={"/assets/logo.png"}
+          className="w-[150px]"
+          width={150}
+          height={200}
+          alt=""
+        />
       </figure>
 
-      <div className='absolute z-20 left-0 top-0 w-[200px] h-[100px]'>
-        <figure className='absolute -left-12 -top-[40px]'>
-          <Image src={'/assets/tl-01.png'} className='w-[150px]' width={150} height={200} alt='' />
+      <div className="absolute z-20 left-0 top-0 w-[200px] h-[100px]">
+        <figure className="absolute -left-12 -top-[40px]">
+          <NextImage
+            src={"/assets/tl-01.png"}
+            className="w-[150px]"
+            width={150}
+            height={200}
+            alt=""
+          />
         </figure>
 
-        <figure className='absolute left-2 top-[10px]'>
-          <Image src={'/assets/tl-02.png'} className='w-[150px]' width={150} height={200} alt='' />
+        <figure className="absolute left-2 top-[10px]">
+          <NextImage
+            src={"/assets/tl-02.png"}
+            className="w-[150px]"
+            width={150}
+            height={200}
+            alt=""
+          />
         </figure>
       </div>
 
-      <div className='absolute z-20 right-0 top-0 w-[200px] h-[100px]'>
-        <figure className='absolute z-20 -right-4 -top-[60px]'>
-          <Image src={'/assets/tr-01.png'} className='w-[150px]' width={150} height={200} alt='' />
+      <div className="absolute z-20 right-0 top-0 w-[200px] h-[100px]">
+        <figure className="absolute z-20 -right-4 -top-[60px]">
+          <NextImage
+            src={"/assets/tr-01.png"}
+            className="w-[150px]"
+            width={150}
+            height={200}
+            alt=""
+          />
         </figure>
 
-        <figure className='absolute -right-[40px] -top-[10px]'>
-          <Image src={'/assets/tr-02.png'} className='w-[150px]' width={150} height={200} alt='' />
+        <figure className="absolute -right-[40px] -top-[10px]">
+          <NextImage
+            src={"/assets/tr-02.png"}
+            className="w-[150px]"
+            width={150}
+            height={200}
+            alt=""
+          />
         </figure>
       </div>
 
-      <section className='w-full h-full p-5 absolute'>
-        <div className='w-full h-full relative bg-[#F7F3F0] rounded-2xl border overflow-hidden'>
+      <section className="w-full h-full p-5 absolute">
+        <div className="w-full h-full relative bg-[#F7F3F0] rounded-2xl border overflow-hidden">
           <CustomUiExample />
         </div>
       </section>
 
-      <div className='absolute bottom-5 left-1/2 -translate-x-1/2'>
-        <button type='button'>Wauasdasdas</button>
-      </div>
+      {/*<div className="absolute bottom-5 z-20 left-1/2 -translate-x-1/2">*/}
+      {/*  <button type="button">Wauasdasdas</button>*/}
+      {/*</div>*/}
 
-      <div className='absolute select-none bottom-[21px] left-[21px] rounded-2xl overflow-hidden z-20 h-[90px] w-[320px]'>
-        <Marquee pauseOnHover={true} gradient={true} speed={20} className={"h-full w-full"} gradientColor={"#F9FAFB"}>
-          I can be a React component, multiple React components, or just some text.
+      <div className="absolute select-none bottom-[21px] left-[21px] rounded-2xl overflow-hidden z-20 h-[90px] w-[320px]">
+        <Marquee
+          pauseOnHover={true}
+          gradient={true}
+          speed={20}
+          className={"h-full w-full"}
+          gradientColor={"#F9FAFB"}
+        >
+          I can be a React component, multiple React components, or just some
+          text.
         </Marquee>
       </div>
 
-      <div className='absolute bottom-0 right-0 flex flex-col gap-3'>
-        {Array.from({ length: 4 }).map((_, i) => (
-          <div className='flex gap-2 items-center' key={i}>
-            <div className='flex'>
-              {Array.from({ length: 4 }).map((_, i) => (
-                <button type='button' key={i} className='w-12 rounded-full aspect-square bg-red-400'>
-                  Wau
-                </button>
-              ))}
-            </div>
-            <button type='button' className='w-12 rounded-full aspect-square bg-red-400'>
-              Wau
-            </button>
-          </div>
-        ))}
-      </div>
+      {/*<div className="absolute bottom-0 z-20 right-0 flex flex-col gap-3">*/}
+      {/*  {Array.from({ length: 4 }).map((_, i) => (*/}
+      {/*    <div className="flex gap-2 items-center" key={i}>*/}
+      {/*      <div className="flex">*/}
+      {/*        {Array.from({ length: 4 }).map((_, i) => (*/}
+      {/*          <button*/}
+      {/*            type="button"*/}
+      {/*            key={i}*/}
+      {/*            className="w-12 rounded-full aspect-square bg-red-400"*/}
+      {/*          >*/}
+      {/*            Wau*/}
+      {/*          </button>*/}
+      {/*        ))}*/}
+      {/*      </div>*/}
+      {/*      <button*/}
+      {/*        type="button"*/}
+      {/*        className="w-12 rounded-full aspect-square bg-red-400"*/}
+      {/*      >*/}
+      {/*        Wau*/}
+      {/*      </button>*/}
+      {/*    </div>*/}
+      {/*  ))}*/}
+      {/*</div>*/}
     </main>
   )
 }
